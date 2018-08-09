ae69d4089de1ae9fde2d9d4955431a4b30d7cf8a
MBx85 <brand-markus@web.de>
2018-08-09 18:13:20 +0200

    added stuff to KIM

--- SpringRESTKIMService/src/main/java/application/KIM.java
+++ SpringRESTKIMService/src/main/java/application/KIM.java
@@ -1,8 +1,13 @@
 package application;
 
+import java.util.Date;
+
 public class KIM {
 	private String kim;
-	
+	private String vorname;
+	private String nachname;
+	private Date geburtstag;
+		
 	public KIM (String kim) {
 		this.kim = kim;
 	}
--- SpringRESTKIMService/src/main/java/application/KIMController.java
+++ SpringRESTKIMService/src/main/java/application/KIMController.java
@@ -2,15 +2,13 @@ package application;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class KIMController {
 
 	@RequestMapping("/KIM/{kim}")
-	public KIM kim(@PathVariable("kim"/*, defaultValue = "World"*/) String kim) {
+	public KIM kim(@PathVariable("kim") String kim) {
 		return new KIM(kim);
 	}
 }
