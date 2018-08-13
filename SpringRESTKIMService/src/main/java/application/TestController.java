5028ca784fed5e3c30ad618fe3d88f927e42886a
MBx85 <brand-markus@web.de>
2018-08-13 17:08:19 +0200

    Test Controller updated
    Signed-off-by: MBx85 <brand-markus@web.de>

--- SpringRESTKIMService/src/main/java/application/TestController.java
+++ SpringRESTKIMService/src/main/java/application/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-	private static final String testString = "This is the KIM REST Service";
+	private static final String testString = "This is the KIM REST Service!";
 
 	@RequestMapping("/test")
 	public String getTestMessage() {
