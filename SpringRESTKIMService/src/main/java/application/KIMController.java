5aeaecec72fc2b97d354f33de93667fe91804c3d
MarkuB2 <MarkuB2@CSTRL0051186877.destr.corpintra.net>
2018-08-13 15:03:50 +0200

    changes on kimcontroller

--- SpringRESTKIMService/src/main/java/application/KIMController.java
+++ SpringRESTKIMService/src/main/java/application/KIMController.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.logging.Logger;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,12 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class KIMController {
 
+	private static final Logger log = Logger.getLogger( KIMController.class.getName() );
+	
 	@RequestMapping(value = "/KIM/{kim}", method = RequestMethod.GET)
 	public KIM kim(@PathVariable("kim") String kim) {
+		log.info("KIM " + kim + " requested");
 		return new KIM(kim);
 	}
 	
 	@RequestMapping(value = "/KIM/{kim}", method = RequestMethod.PUT)
-		public void doNothing() {
+		public void doNothing(@PathVariable("kim") String kim) {
+		log.info("Modified KIM " + kim);
 	}
 }
