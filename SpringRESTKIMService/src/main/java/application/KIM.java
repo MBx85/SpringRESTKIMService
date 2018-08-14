c77d1b646e6049ed4c81059980c7aad16d57aec9
MarkuB2 <MarkuB2@CSTRL0051186877.destr.corpintra.net>
2018-08-14 17:49:48 +0200

    lots of changes

--- SpringRESTKIMService/src/main/java/application/KIM.java
+++ SpringRESTKIMService/src/main/java/application/KIM.java
@@ -2,11 +2,16 @@ package application;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class KIM {
 	private String kim;
 	private String vorname;
 	private String nachname;
 	private Date geburtstag;
+	private Date saveDate; // (UpdateDateTime)
 
 	public KIM() {
 	}
@@ -14,8 +19,10 @@ public class KIM {
 	public KIM(String kim) {
 		KIM TempKimObject = KIMDataFileReader.GetKimFromFile(kim);
 		this.kim = TempKimObject.getKIM();
-		this.vorname = TempKimObject.getVorname();
-		this.nachname = TempKimObject.getNachname();
+		if (this.kim != null) {
+			this.vorname = TempKimObject.getVorname();
+			this.nachname = TempKimObject.getNachname();
+		}
 	}
 
 	public void setKim(String value) {
--- SpringRESTKIMService/src/main/java/application/KIMController.java
+++ SpringRESTKIMService/src/main/java/application/KIMController.java
@@ -1,9 +1,11 @@
 package application;
 
+import java.util.Enumeration;
 import java.util.logging.Logger;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,7 @@ public class KIMController {
 
 	private static final Logger log = Logger.getLogger( KIMController.class.getName() );
 	
+	//BELOW IS FOR HTTP GET
 	@CrossOrigin(origins = "http://localhost:8092")
 	@RequestMapping(value = "/KIM/{kim}", method = RequestMethod.GET)
 	public KIM kim(@PathVariable("kim") String kim) {
@@ -20,8 +23,18 @@ public class KIMController {
 		return new KIM(kim);
 	}
 	
+	//BELOW IS FOR HTTP PUT
+	@CrossOrigin(origins = "http://localhost:8092")
 	@RequestMapping(value = "/KIM/{kim}", method = RequestMethod.PUT)
-		public void doNothing(@PathVariable("kim") String kim) {
+		public void PutKIM(@PathVariable("kim") String kim) {
+		/* Get KIM From File; if it is there --> overwrite; if not --> create*/
+		/*KIM FileKIM = new KIM(kim);
+		if(FileKIM.getKIM() == null) {
+			// Append to file
+		}
+		else {
+			// overwrite
+		}*/
 		log.info("Modified KIM " + kim);
 	}
 }
