3e5f19c21f8086757b249991a37e37ce04d58f71
MarkuB2 <MarkuB2@CSTRL0051186877.destr.corpintra.net>
2018-08-17 16:04:04 +0200

    HTML reusable now

--- SpringRESTKIMService/src/main/java/application/ui/IndexController.java
+++ SpringRESTKIMService/src/main/java/application/ui/IndexController.java
@@ -1,14 +1,31 @@
 package application.ui;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 
 @Controller
 public class IndexController {
 
 	@GetMapping("/index")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("fragment", "/fragments/main");
+		model.addAttribute("fragmentElement", "main");
 		return "index";
+	}
+
+	@GetMapping("/mitarbeiter")
+	public String mitarbeiter(Model model) {
+		model.addAttribute("fragment", "fragments/mitarbeiterAnzeigen");
+		model.addAttribute("fragmentElement", "mitarbeiterAnzeigen");
+		return "index";
+	}
+	
+	@GetMapping("/RefreshKIMs")
+	public String refreshKIMs(Model model) {
+		model.addAttribute("fragment", "fragments/kimAktualisieren");
+		model.addAttribute("fragmentElement", "kimAktualisieren");
+		return "index";
+		
 	}
 }
--- SpringRESTKIMService/src/main/java/application/ui/TestHTMLController.java
+++ SpringRESTKIMService/src/main/java/application/ui/TestHTMLController.java
@@ -1,13 +1,16 @@
 package application.ui;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestHTMLController {
 
 	@GetMapping("/testhtml")
-	public String refreshKIMs() {
+	public String getTestHTML(Model model) {
+		model.addAttribute("fragment", "/fragments/testfragment");
+		model.addAttribute("fragmentElement", "testfragment");
 		return "TestHTML";
 	}
 }
