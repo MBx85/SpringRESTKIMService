b28da3ebd13259532255865a8154be6a400b634e
MarkuB2 <MarkuB2@CSTRL0051186877.destr.corpintra.net>
2018-09-06 18:03:53 +0200

    changed playground

--- SpringRESTKIMService/src/main/java/application/Playground.java
+++ SpringRESTKIMService/src/main/java/application/Playground.java
@@ -1,51 +1,23 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-
-import csvStuff.CSVSupporter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Playground {
 
 	public static void main(String[] args) {
-		final String filepath = "C:\\Testing\\Kimdata.csv";
-		final String filepath2 = "C:\\Testing\\Kimdata2.csv";
-		final String csvDivider = ";";
-		File file2Read = new File(filepath);
-		File file2Write = new File(filepath2);
-		
-		
-		try {
-			FileReader fileReader = new FileReader(filepath);
+		String dateString = "1985-08-26T22:06:33";
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
 
-			// Always wrap FileReader in BufferedReader.
-			BufferedReader bufferedReader = new BufferedReader(fileReader);
-			FileWriter fw = new FileWriter(filepath2);
-						
-			String line = null;
-			int linecounter = 0;
-			while (((line = bufferedReader.readLine()) != null)) {
-				fw.write(line+ "\r\n");
-				if (linecounter != 0) // because header line should not be read
-				{
-					String[] values = line.split(csvDivider);
-					System.out.println("Vorname " + values[CSVSupporter.GetAttributeCSVArrayPos("vorname")]);
-				}
-				linecounter++;
-			}
-			fw.close();
-			// Always close files.
-			bufferedReader.close();
-			file2Write.renameTo(file2Read);
-			FileWriter CleanUpper = new FileWriter(filepath2);
-			CleanUpper.write("");
-			CleanUpper.close();
-			
-		} catch (Exception e) {
-			System.out.println("EXCEPTION CAUGHT" + e.getMessage());
+		
+		try {Date date = sdf.parse(dateString);
+		System.out.println(date);
 		}
+		catch(Exception e) {
+			
+		}
+		}
+	
 	}
 
-}
+
