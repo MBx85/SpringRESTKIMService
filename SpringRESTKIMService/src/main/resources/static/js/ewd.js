3034fe9712a8bd60d4fbd502944a816bbb2ea306
MBx85 <brand-markus@web.de>
2019-02-25 22:35:38 +0100

    ui changes

--- SpringRESTKIMService/src/main/resources/static/js/ewd.js
+++ SpringRESTKIMService/src/main/resources/static/js/ewd.js
@@ -31,7 +31,7 @@ function RefreshKims() {
 }
 
 function SaveRestService() {
-     var service = new Object();
+    var service = new Object();
     service.id = document.getElementById("ServiceID").value;
     service.endpoint = document.getElementById("ServiceEndpoint").value;
     service.version = document.getElementById("ServiceVersion").value;
@@ -40,6 +40,49 @@ function SaveRestService() {
     xmlHttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xmlHttp.send(JSON.stringify(service));
 }
+
+function GetAllRestServices(callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) { }
+        callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", "/REST/Services", true);
+    xmlHttp.send(null);
+}
+
+function PopulateServicesDropdown(){
+    GetAllRestServices(WriteServiceIDsToDropdown);
+}
+
+function WriteServiceIDsToDropdown(response) {
+    var dropdown = document.getElementById("ServiceIdSelect");
+    while (dropdown.firstChild) {
+        dropdown.removeChild(dropdown.firstChild);
+      }
+      //document.getElementById("RestSource").value = response;
+      var arr = new Array(response);
+      alert(arr.length);
+    /*for (i = 1; i <= 2; i++) {
+        var opt = document.createElement("option");
+        opt.value = i;
+        opt.innerHTML=i;
+        dropdown.appendChild(opt);
+    }*/
+
+
+}
+
+/*
+
+function WriteResSource(){
+    GetAllRestServices(WriteResponseToSourceField);
+}
+
+function WriteResponseToSourceField(response){
+    document.getElementById("RestSource").value = response;
+}
+*/
 
 function alertDate() {
     alert(a);
